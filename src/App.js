183d56faca33c26cdbf46575a8f76712600f9681
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-12-03 15:14:59 +0530

    adding dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter,Routes, Route , Router} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+import React,{useEffect, useState} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
 import PrivateComponent from "./components/PrivateComponents";
 import LoginPage from "./components/LoginPage";
-import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
-import About from "./components/About";
 import TimetableInput from "./components/timetableInput";
-import "./App.css";
 import Dashboard from "./components/Dashboard";
-import Profile from "./components/Profile"
+import Profile from "./components/Profile";
+import { AppProvider } from "./AppProvider/AppProvider";
+import AdminDashBoard from "./pages/AdminDashboard/AdminDashBoard";
+import Navbar from "./components/Navbar";
+import Sem from "./pages/Sem/Sem";
+import Subjects from "./pages/Subjects/Subjects";
 
 function App() {
-  // const [message, setMessage] = useState("");
+  const[branches,setBranches]=useState([])
+  const semester=[1,2,3,4,5,6,7,8]
+  const AllBranches = async (event) => {
+    const authToken = localStorage.getItem("authorization");
+    try {
+      const response = await fetch(
+        "http://localhost:8000/subject/getbranches",
+        {
+          method: "GET",
+          headers: {
+            Authorization: authToken,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setBranches(data.branches)
+      } else {
+        const errorData = await response.json();
+        throw new Error(`Failed to get all branch: ${errorData.message}`);
+      }
+    } catch (err) {
+      console.error("Error fetching branches:", err);
+    }
+  
+};
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8000/message")
-  //     .then((res) => res.json())
-  //     .then((data) => setMessage(data.message));
-  // }, []);
+useEffect(() => {
+  AllBranches();
+}, []);
+
 
   return (
-    
-       
+    <AppProvider>
       <BrowserRouter>
-         <Navbar/>
+        {/* <Navbar/> */}
+        <Routes>
+          {/* PrivateComponent and Signout route */}
+          <Route element={<PrivateComponent />}>
+            <Route path="/Signout"></Route>
+          </Route>
 
+          {/* Other routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/timetable" element={<TimetableInput />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin" element={<AdminDashBoard />} />
+          
 
+          {/* Dynamically generated routes for each branch */}
+          {branches.map((branch, index) => (
+            <Route key={index} path={`/branches/${branch}`} element={<Sem branch={branch} />} />
+            
+          ))}
 
-
-      <Routes>
-        <Route  element={<PrivateComponent/>}>
-              <Route path= '/Signout' ></Route>
-        </Route>
-              <Route path='/login' element={<LoginPage/>}></Route>
-
-       <Route path='/' element={<Home/>}></Route>
-       <Route path= '/Signup' element={<SignUp/>}></Route>
-       {/* <Route path= '/about' element={<About/>}></Route> */}
-       <Route path= '/profile' element={<Profile/>}></Route>
-       <Route path= '/timetable' element={<TimetableInput/>}></Route>
-       <Route path= '/dashboard' element={<Dashboard/>}></Route>
-  
-      </Routes>
-           
-      
+          {/* Dynamically generate routes for each semester under each branch */}
+          {branches.map((branch, branchIndex) => (
+            semester.map((sem, semIndex) => (
+              <Route
+                key={semIndex}
+                path={`/branches/${branch}/${sem}`}
+                element={<Subjects branch={branch} semester={sem} />}
+              />
+            ))
+          ))}
+        </Routes>
       </BrowserRouter>
-       
-      
-    
+    </AppProvider>
   );
 }
 
-export default App
+export default App;
