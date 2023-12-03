65ab7a74ca4e98c5b010e4b264dbd22c29e6d847
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-12-03 15:06:56 +0530

    fix: adaptable for both user and admins and adding token authorization

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,39 +1,77 @@
 import React, { useState, useEffect } from "react";
-
-import ReactDOM from "react-dom";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const Navigate=useNavigate()
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    email: "",
+    Confirmpassword: "",
+  });
+  const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  });
 
-  const navigate=useNavigate()
-    useEffect(()=>{
-      const auth=localStorage.getItem('user')
-      if(auth){
-         navigate("/")
-      }
-    })
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
+  const determineUserRole = (email) => {
+    if (email === "admin@nith.ac.in") {
+      return "admin";
+    } else {
+      return "user";
+    }
+  };
 
   const handlesubmit = async () => {
-    console.warn(name, email, password);
-    const phoneNumber=null;
-    const address=null
-    let result = await fetch(`${process.env.REACT_APP_API_URL}/Signup`, {
-      method: "post",
-      body: JSON.stringify({ name, email, password,phoneNumber,address}),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    result = await result.json();
-    console.log(result);
-    localStorage.setItem("user",JSON.stringify(result))
-    if(result){
-           Navigate('/login')
+    if (formData.password !== formData.Confirmpassword) {
+      alert("Password didn't match");
+      return;
+    }
+    const role = determineUserRole(formData.email);
+    console.log(role)
+    const phoneNumber = null;
+    const address = null;
+    console.log(formData)
+    try {
+      const response = await fetch(
+        `http://localhost:8000/auth/signup?role=${role}`,
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formData.name,
+            email: formData.email,
+            password: formData.password,
+            phoneNumber: phoneNumber,
+            address: address,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      if (response.status === 201) {
+        localStorage.setItem("authorization", `Bearer ${data.authToken}`);
+        alert(data.message);
+        navigate("/login");
+      } else {
+        console.log(data);
+        throw new Error(data.message);
+      }
+    } catch (err) {
+      console.log("Error during signup");
+      alert(err);
     }
   };
   return (
@@ -52,33 +90,48 @@ export default function SignUp() {
           <div className="form-group mt-3">
             <label>Full Name</label>
             <input
-              type="name"
+              name="name"
+              type="text"
               className="form-control mt-1"
               placeholder="e.g Jane Doe"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={formData.name}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
+              name="email"
               type="email"
               className="form-control mt-1"
               placeholder="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={formData.email}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
             <input
+             name="password"
               type="password"
               className="form-control mt-1"
               placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Confirm Password</label>
+            <input
+              name="Confirmpassword"
+              type="password"
+              className="form-control mt-1"
+              placeholder="Confirm Password"
+              value={formData.Confirmpassword}
+              onChange={handleChange}
               required
             />
           </div>
