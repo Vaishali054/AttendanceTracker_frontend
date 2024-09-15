68cc78f9854813828f5fef4498413f77ad2ebb10
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-09-15 13:19:40 +0530

    add: dynamic edit/add button for each category

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,29 +14,102 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import School from '@mui/icons-material/School';
-import { drawerWidth, DrawerHeader, AppBar} from '../../constants/constant';
-
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DegreeForm from '../DegreeForm/DegreeForm';
+import SubjectForm from '../SubjectForm/SubjectForm';
+import TimeTableForm from '../TimeTableForm/TimeTableForm'
+import TeacherForm from '../TeacherFrom/TeacherForm';
+import DepartmentForm from '../DepartmentForm/DeaprtmentForm';
+import { drawerWidth, DrawerHeader, AppBar } from '../../constants/constant';
+import {Dialog, DialogActions, DialogContent, DialogTitle} from '@mui/material';
 
 const navItems = [
   { text: 'Teacher', path: '/admindashboard/teacher' },
   { text: 'Subject', path: '/admindashboard/subject' },
   { text: 'Department', path: '/admindashboard/department' },
   { text: 'Timetable', path: '/admindashboard/timetable' },
-  { text: 'Degree', path: '/admindashboard/degree' }
+  { text: 'Degree', path: '/admindashboard/degree' },
 ];
 
 export default function DrawerVariant(props) {
   const theme = useTheme();
-  const {handleDrawerOpen, handleDrawerClose, open}=props
+  const [open, setOpen] = React.useState(true);
+  const [formType, setFormType] = React.useState(null); // State to manage which form to show
+  const location = useLocation();
+
+  const getAddButtonLabel = () => {
+    switch (location.pathname) {
+      case '/admindashboard/teacher':
+        return 'Add New Teacher';
+      case '/admindashboard/subject':
+        return 'Add New Subject';
+      case '/admindashboard/department':
+        return 'Add New Department';
+      case '/admindashboard/timetable':
+        return 'Add New Timetable';
+      case '/admindashboard/degree':
+        return 'Add New Degree';
+      default:
+        return null;
+    }
+  };
+
+  const handleAddClick = () => {
+    switch (location.pathname) {
+      case '/admindashboard/teacher':
+        setFormType('teacher');
+        break;
+      case '/admindashboard/subject':
+        setFormType('subject');
+        break;
+      case '/admindashboard/department':
+        setFormType('department');
+        break;
+      case '/admindashboard/timetable':
+        setFormType('timetable');
+        break;
+      case '/admindashboard/degree':
+        setFormType('degree');
+        break;
+      default:
+        break;
+    }
+  };
+
+  const renderForm = () => {
+    switch (formType) {
+      case 'teacher':
+        return <TeacherForm/>;
+      case 'subject':
+        return <SubjectForm />;
+      case 'department':
+        return <DepartmentForm/>;
+      case 'timetable':
+        return <TimeTableForm />;
+      case 'degree':
+        return <DegreeForm />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
-      <AppBar position="fixed" open={open}>
+      <AppBar 
+        position="fixed" 
+        open={open}
+        sx={{
+          display: 'flex', 
+          justifyContent: 'space-between', 
+          backgroundColor: '#1976d2'
+        }}
+      >
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
+            onClick={() => setOpen(true)}
             edge="start"
             sx={{
               mr: 2,
@@ -45,11 +118,28 @@ export default function DrawerVariant(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+          <Typography 
+            variant="h6" 
+            noWrap 
+            component="div" 
+            sx={{ flexGrow: 1, color: '#fff' }}
+          >
             Admin Dashboard
           </Typography>
+          {getAddButtonLabel() && (
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ marginLeft: 'auto', backgroundColor: '#fff', color: '#1976d2' }}
+              startIcon={<AddIcon />}
+              onClick={handleAddClick}
+            >
+              {getAddButtonLabel()}
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
+
       <Drawer
         sx={{
           width: drawerWidth,
@@ -64,17 +154,18 @@ export default function DrawerVariant(props) {
         open={open}
       >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
+          <IconButton onClick={() => setOpen(false)}>
             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </DrawerHeader>
+
         <List>
           {navItems.map((item) => (
             <ListItem key={item.text}>
               <Link to={item.path} style={{ textDecoration: 'none', color: 'inherit' }}>
                 <ListItemButton>
                   <ListItemIcon>
-                    <School />
+                    <School style={{ color: '#1976d2' }} />
                   </ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItemButton>
@@ -83,6 +174,22 @@ export default function DrawerVariant(props) {
           ))}
         </List>
       </Drawer>
+
+      {/* Dialog for form */}
+      <Dialog
+        open={!!formType}
+        onClose={() => setFormType(null)}
+      >
+        <DialogTitle>{`Add New ${getAddButtonLabel().split(' ')[2]}`}</DialogTitle>
+        <DialogContent>
+          {renderForm()}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setFormType(null)} color="secondary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
