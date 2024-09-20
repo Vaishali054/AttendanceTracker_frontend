3498a399400f0749c87a821d2ea8085c4ac91b8d
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-09-21 01:11:49 +0530

    load : department & degrees Offered

---
+++ src/api/fetchcmds.jsx
@@ -0,0 +1,16 @@
+import { AxiosError } from "axios";
+import axiosInstance from "../infra/axiosInstance";
+
+export const getDepartments=async()=>{
+    try{
+        const response =await axiosInstance.get('/admin/department/get');
+        console.log(response.data)
+        return response.data.departments;
+    }
+    catch(error){
+        if (error instanceof AxiosError) {
+            return error.response?.data || error.message;
+          }
+          throw error;
+    }
+}
--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -104,3 +104,16 @@ export const data = [
       return 'teacher';
     }
   };
+
+
+export const SignUpContainer = styled(Stack)(({ theme }) => ({
+  height: '100vh',
+  padding: 1,
+  backgroundImage:
+    'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+  backgroundRepeat: 'no-repeat',
+  ...theme.applyStyles('dark', {
+    backgroundImage:
+      'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+  }),
+}));
